72495eb0aec733439aa70f22068aec3b087083ff
Eunyeoul Lee <yiio08@naver.com>
2022-09-09 17:43:24 +0900

    [BE][feat]#2 Create node server

---
+++ backend/data.js
@@ -0,0 +1,58 @@
+const data = {
+  products: [
+    {
+      name: '나이트로 바닐라 크림',
+      slug: 'nitro-vanilla-cream',
+      category: '콜드브루',
+      image: '/images/nitro-vanilla-cream.jpg',
+      price: 6.1,
+      countInStock: 10,
+      brand: '스타벅스',
+      rating: 4.5,
+      numReviews: 10,
+      description:
+        '부드러운 목넘김의 나이트로 커피와 바닐라 크림의 매력을 한번에 느껴보세요!',
+    },
+    {
+      name: '에스프레소 콘 파나',
+      slug: 'espresso-con-panna',
+      category: '에스프레소',
+      image: '/images/espresso-con-panna.jpg',
+      price: 4.2,
+      countInStock: 20,
+      brand: '스타벅스',
+      rating: 4.0,
+      numReviews: 10,
+      description:
+        '신선한 에스프레소 샷에 풍부한 휘핑크림을 얹은 커피 음료로서, 뜨거운 커피의 맛과 차갑고 달콤한 생크림의 맛을 같이 즐길 수 있는 커피 음료',
+    },
+    {
+      name: '블랙 티 레모네이드 피지오',
+      slug: 'black-tea-lemonade-starbucks-fizzio',
+      category: '스타벅스 피지오',
+      image: '/images/black-tea-lemonade-starbucks-fizzio.jpg',
+      price: 5.4,
+      countInStock: 15,
+      brand: '스타벅스',
+      rating: 4.5,
+      numReviews: 14,
+      description:
+        '진하게 우린 블랙 티와 상큼한 레모네이드를 더한 청량감 있는 음료입니다. 목 넘김이 좋은 가벼운 탄산이 첨가되어 깔끔하게 즐길 수 있습니다.',
+    },
+
+    {
+      name: '그린 애플 캐모마일 티',
+      slug: 'green-apple-chamomile-tea',
+      category: '티',
+      image: '/images/green-apple-chamomile-tea.jpg',
+      price: 6.1,
+      countInStock: 5,
+      brand: '스타벅스',
+      rating: 4.5,
+      numReviews: 10,
+      description:
+        '국내산 사과로 만든 베이스에 은은한 캐모마일 티로 향긋한 풍미를 즐길 수 있는 티 베리에이션 음료. 국내산 농산물을 활용한 지역 상생 음료입니다.',
+    },
+  ],
+};
+export default data;
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
-// node_modules에 있는 express 관련 파일을 가져온다.
-const express = require('express');
+import data from './data.js';
+import express from 'express';
 
 // express는 함수이기 때문에, 반환값을 변수에 저장한다.
 const app = express();
 
-const connectDB = require('./database/db');
-
-connectDB();
+app.get('/api/products', (req, res) => {
+  res.send(data.products);
+});
 
 // PORT 값이 설정돼 있지 않다면, 5000번을 사용
 const port = process.env.PORT || 5000;
